dd0a59744e3233543f15668873e394ff87d4cc6f
JaxNext <qianjunyinggo@gmail.com>
2025-11-09 18:53:08 +0800

    Update beijing.js to add new workshop entries with project links and GitHub profiles for multiple authors.

--- data/beijing.js
+++ data/beijing.js
@@ -1,7 +1,33 @@
 export default [
     {
-        link: 'https://example.com',
-        authorName: 'Developer Name',
+        link: 'https://gdg.bwte.cn/',
+        authorName: '喵喵',
+        authorGitHubLink: '',
+    },
+    {
+        link: 'https://neo-of-matrix.github.io/devfest-chrome-devtools-workshop/',
+        authorName: 'Dick',
+        authorGitHubLink: '',
+    },
+    {
+        link: 'https://wangyonghong.github.io/DevFestWeb/',
+        authorName: 'wangyonghong',
+        authorGitHubLink: 'https://github.com/wangyonghong',
+    },
+    {
+        link: 'https://meyqy.github.io/devtools-mcp-workshop/',
+        authorName: 'meyqy',
+        authorGitHubLink: 'https://github.com/meyqy',
+    },
+    {
+        link: 'https://crocoocn.github.io/DevFest2025Beijing-Web/index.html',
+        authorName: 'crocoocn',
+        authorGitHubLink: 'https://github.com/crocoocn',
+    },
+    {
+        link: 'https://fa1c0nsec.github.io/2025-gdgdevfest-beijing-chromedevtools-workshop-fa1c0n.github.io/',
+        authorName: 'fa1c0nsec',
+        authorGitHubLink: 'https://github.com/fa1c0nsec',
     }
 ]
 
