d8f58c3673b6447c4e224f332c6da1c51ddaa12a
JaxNext <qianjunyinggo@gmail.com>
2025-11-16 18:21:22 +0800

    Add workshop entries in shenzhen.js with project links and GitHub profiles for multiple authors.

--- data/shenzhen.js
+++ data/shenzhen.js
@@ -1,7 +1,78 @@
 export default [
     {
-        link: 'https://example.com',
-        authorName: 'Developer Name',
+        link: 'https://isxuefang77-hue.github.io/DevFestShenzhen2025/index.html',
+        authorName: 'Lucas',
+        authorGitHubLink: 'https://github.com/isxuefang77-hue',
+    },
+    {
+        link: 'https://odex21.github.io/devfest-devtools-mcp-workshop/',
+        authorName: 'odex21',
+        authorGitHubLink: 'https://github.com/odex21',
+    },
+    {
+        link: 'https://normal-coder.github.io/chrome-devtool-shenzhen-202511-adk-show/',
+        authorName: 'normal-coder',
+        authorGitHubLink: 'https://github.com/normal-coder',
+    },
+    {
+        link: 'https://devtools-mcp-website.pages.dev/',
+        authorName: 'devtools-mcp-website',
+        authorGitHubLink: 'https://github.com/devtools-mcp-website',
+    },
+    {
+        link: 'https://jasonthewhale.github.io/devfest-chrome-devtools-mcp',
+        authorName: 'jasonthewhale',
+        authorGitHubLink: 'https://github.com/jasonthewhale',
+    },
+    {
+        link: 'https://robinmo.github.io/devtools-mcp-site/',
+        authorName: 'robinmo',
+        authorGitHubLink: 'https://github.com/robinmo',
+    },
+    {
+        link: 'https://iccyyxx.github.io/devtools-mcp-website/',
+        authorName: 'iccyyxx',
+        authorGitHubLink: 'https://github.com/iccyyxx',
+    },
+    {
+        link: 'https://leon-algo.github.io/devfest-devtools-mcp-website/art-index.html',
+        authorName: 'leon-algo',
+        authorGitHubLink: 'https://github.com/leon-algo',
+    },
+    {
+        link: 'https://crackcatcher.github.io/devfest-mcp-workshop/',
+        authorName: 'crackcatcher',
+        authorGitHubLink: 'https://github.com/crackcatcher',
+    },
+    {
+        link: 'https://lukidesign.github.io/luki-dev/',
+        authorName: 'lukidesign',
+        authorGitHubLink: 'https://github.com/lukidesign',
+    },
+    {
+        link: 'http://michael2008s.github.io/devfest-workshop-2025/',
+        authorName: 'michael2008s',
+        authorGitHubLink: 'https://github.com/michael2008s',
+    },
+    {
+        link: 'https://alfredguquan.github.io/devtools-mcp-workshop/',
+        authorName: 'alfredguquan',
+        authorGitHubLink: 'https://github.com/alfredguquan',
+    },
+    {
+        link: 'https://mywander.github.io/devtools-mcp-website/',
+        authorName: 'mywander',
+        authorGitHubLink: 'https://github.com/mywander',
+    },
+    {
+        link: 'https://564458428.github.io/devfest-workshop/about-me.html',
+        authorName: '564458428',
+        authorGitHubLink: 'https://github.com/564458428',
+    },
+    {
+        link: 'https://skyfall-j.github.io/devtools-mcp-website/index.html',
+        authorName: 'skyfall-j',
+        authorGitHubLink: 'https://github.com/skyfall-j',
     }
 ]
 
