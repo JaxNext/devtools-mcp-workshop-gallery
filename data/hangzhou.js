35de08fe910841c262438cd33101b4ce1ce8f822
Jax <49943038+JaxNext@users.noreply.github.com>
2025-11-24 02:15:07 +0800

    Remove name from hangzhou.js

--- data/hangzhou.js
+++ data/hangzhou.js
@@ -1,108 +1,90 @@
 export default [
     {
-        name: 'DevFest Journey 2025',
         link: 'https://jaxnext.github.io/devfest-journey-2025/',
         authorName: 'JaxNext',
         authorGitHubLink: 'https://github.com/JaxNext',
     },
     {
-        name: 'My Personal Website',
         link: 'https://sanaapk-jpg.github.io/my-website/index.html',
         authorName: 'sanaapk-jpg',
         authorGitHubLink: 'https://github.com/sanaapk-jpg',
     },
     {
-        name: 'DevFest Hangzhou',
         link: 'https://ryan041001.github.io/DevFest-Hangzhou/',
         authorName: 'ryan041001',
         authorGitHubLink: 'https://github.com/ryan041001',
     },
     {
-        name: 'DevFest HZ Blog',
         link: 'http://blog.julyyu.cn/DevFest-HZ/',
         authorName: 'JulyYu',
         authorGitHubLink: 'https://github.com/lifelikejuly',
     },
     {
-        name: 'MCP Workshop',
         link: 'https://lim-kim930.github.io/mcp/',
         authorName: 'lim-kim930',
         authorGitHubLink: 'https://github.com/lim-kim930',
     },
     {
-        name: 'GDG DevTools',
         link: 'https://lehemyang.github.io/GdgDevtools/',
         authorName: 'lehemyang',
         authorGitHubLink: 'https://github.com/lehemyang',
     },
     {
-        name: 'Net Dairy Website',
         link: 'https://nanamiakari.github.io/netdairy-website/',
         authorName: 'nanamiakari',
         authorGitHubLink: 'https://github.com/nanamiakari',
     },
     {
-        name: 'Chrome DevTools Test',
         link: 'https://mahone3297.github.io/test-chrome-devtools-mcp/',
         authorName: 'mahone3297',
         authorGitHubLink: 'https://github.com/mahone3297',
     },
     {
-        name: 'DevFest Workshop 2025',
         link: 'https://bhctls.github.io/devfest-workshop-2025/',
         authorName: 'bhctls',
         authorGitHubLink: 'https://github.com/bhctls',
     },
     {
-        name: 'Google DevFest Chrome DevTools',
         link: 'https://zhaojingnan040813.github.io/Google_DevFest_Chrome_DevTools_MCP/',
         authorName: 'zhaojingnan040813',
         authorGitHubLink: 'https://github.com/zhaojingnan040813',
     },
     {
-        name: 'DevFest Workshop',
         link: 'https://tricial12.github.io/devfest-workshop/',
         authorName: 'tricial12',
         authorGitHubLink: 'https://github.com/tricial12',
     },
     {
-        name: 'DevTools MCP Frontend',
         link: 'https://giftedscout.github.io/DevTools-MCP-Frontend/',
         authorName: 'giftedscout',
         authorGitHubLink: 'https://github.com/giftedscout',
     },
     {
-        name: 'Google Dev Project',
         link: 'https://geekgallifrey.github.io/Google-Dev/',
         authorName: 'geekgallifrey',
         authorGitHubLink: 'https://github.com/geekgallifrey',
     },
     {
-        name: 'Google Dev Workshop',
         link: 'https://guinanlin.github.io/google-dev/index.html',
         authorName: 'guinanlin',
         authorGitHubLink: 'https://github.com/guinanlin'
     },
     {
-        name: 'DevFest Workshop Site 2025',
         link: 'https://sjx1943.github.io/devfest-workshop-site-2025',
         authorName: 'sjx1943',
         authorGitHubLink: 'https://github.com/sjx1943'
     },
     {
-        name: 'Google Dev Crazy',
         link: 'https://yangjiwen.github.io/google_dev_crazy/',
         authorName: 'yangjiwen',
         authorGitHubLink: 'https://github.com/yangjiwen'
     },
     {
-        name: 'DevFest 2025 HZ',
         link: 'https://zhonglin0730.github.io/DevFest2025-HZ/',
         authorName: 'zhonglin0730',
         authorGitHubLink: 'https://github.com/zhonglin0730'
     },
     {
-        name: 'DevFest Workshop Website',
         link: 'https://cycle001.github.io/devfest-workshop-website/',
         authorName: 'cycle001',
         authorGitHubLink: 'https://github.com/cycle001'
